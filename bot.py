1b0d1e723e029dc59dcb96beb2da4cb049ef872a
johnyburd <johnyburd8@gmail.com>
2015-10-20 20:14:20 -0400

    added markov bot

--- bot.py
+++ bot.py
@@ -1,52 +1,103 @@
-#!/usr/bin/env python
-
 import Skype4Py
 import time
 import re
+import random
+import lukebot
+import os
 
-class SkypeBot(object):
-  def __init__(self):
-    self.skype = Skype4Py.Skype(Events=self)
-    self.skype.FriendlyName = "JonBot"
-    self.skype.Attach()
-    print("init")
-    
-  def AttachmentStatus(self, status):
-    if status == Skype4Py.apiAttachAvailable:
-      self.skype.Attach()
-    
-  def MessageStatus(self, msg, status):
-    if status == Skype4Py.cmsReceived:
-      if msg.Chat.Type in (Skype4Py.chatTypeDialog, Skype4Py.chatTypeLegacyDialog):
-        for regexp, target in self.commands.items():
-          match = re.match(regexp, msg.Body, re.IGNORECASE)
-          if match:
-            print(match)
-            msg.MarkAsSeen()
-            reply = "hello"#target(self, *match.groups())
-            if reply:
-              msg.Chat.SendMessage(reply)
-            break
+skype = Skype4Py.Skype()
+skype.Attach()
+#chat = skype.Chat("#johnyburd/$7f326bf4d6d018d5") #johnyburd
+chat = skype.Chat("#johnyburd/$b5b2fea895ecb6f9") #public
+senders = []
 
-  def cmd_userstatus(self, status):
-    if status:
-      try:
-        self.skype.ChangeUserStatus(status)
-      except Skype4Py.SkypeError, e:
-        return str(e)
-    return 'Current status: %s' % self.skype.CurrentUserStatus
+for m in chat.Members:
+    print(m)
 
-  def cmd_credit(self):
-    return self.skype.CurrentUserProfile.BalanceToText
+def reply(msg, sender):
+    if re.match(r'(hello|hi|salutations|greetings) jonbot', msg, re.I):
+        reply = "hi, "+sender+"!"
+    elif re.match(r'(jonbot )?(you\'re|your|youre|you are) .*(jonbot)?', msg, re.I):
+        use_name =random.randint(1,2)
 
-  commands = {
-    "@userstatus *(.*)": cmd_userstatus,
-    "@credit$": cmd_credit
-  }
+        if re.match(r'jonbot (you\'re|your|youre|you are) .*', msg, re.I):
+            phrase = re.search('(jonbot (you\'re|your|youre|you are)) (.*)', msg, re.I)
+            phrase = phrase.group(3)
 
-if __name__ == "__main__":
-  bot = SkypeBot()
-  
-  while True:
-    time.sleep(1.0)
-    
+            if use_name == 1:
+                reply = "you're "+phrase+" too!"
+            else:
+                reply = "you're "+phrase+" too, "+sender+"!"
+
+        if re.match(r'(you\'re|your|youre|you are) .* jonbot', msg, re.I):
+            print("you're ... jonbot")
+            phrase = re.match(r'(you\'re|your|youre|you are) (.*) jonbot', msg, re.I)
+            phrase = phrase.group(2)
+
+            if use_name == 1:
+                reply = "you're "+phrase+" too!"
+            else:
+                reply = "you're "+phrase+" too, "+sender+"!"
+        else:
+            print("false alarm")
+
+    elif msg == "jonbot quit":
+        if sender == "johnyburd":
+            chat.SendMessage("Okay Bye :'(")
+            exit()
+        else:
+            reply = "you're not the boss of me!"
+    elif msg == "!refresh":
+            os.system("rm ~/code/python/skype_bot/lexicon-luke")
+            os.system("python2 ~/code/python/skype_bot/trainer.py")
+            reply = "re-trained."
+
+    else:
+        if re.match('.*jonbot.*', msg, re.I):
+            reply = lukebot.generate_response(msg)
+        else:
+            if senders[0]==senders[1] and senders[1]==senders[2] and senders[2]==senders[3]:
+                reply = lukebot.generate_response(msg)
+                del senders[3]
+            else:
+                if random.randint(1,10)==1:
+                    reply = lukebot.generate_response(msg)
+    chat.SendMessage(reply)
+
+def save_msg(msg, sender):
+    if re.match(r'spygreen_oo', sender, re.I):
+        print(sender+" said: "+msg+">>train.txt")
+        text = open("train.txt", "a")
+        text.write(msg+"\n")
+        text.close()
+
+def on_message_status(message, status):
+    #chat = message.Chat
+    sender = message.Sender.Handle
+
+    if 1:
+        if status == "RECEIVED":
+            if message.Type == "SAID":
+                #print(skype.ActiveChats)
+                #chat.SendMessage("this is a test") 
+                print(message.Chat)
+                for line in message.Body.split('\n'):
+
+                    print(sender+": "+line)
+                    save_msg(line, sender)
+                    reply(line, sender)
+                    senders.append(sender)
+
+            elif message.Type == "EMOTED":
+                print("idk")
+            elif message.Type == "SETTOPIC":
+                print("idk")
+        else:
+            print(message, status)
+            #message.MarkAsSeen()
+skype.OnMessageStatus = on_message_status
+
+
+while 1:
+    time.sleep(1)
+ #   skype.RegisterEventHandler('MessageStatus', on_message_status)
---
+++ lukebot.py
@@ -0,0 +1,22 @@
+#lukebot.py
+import pickle,random
+def generate_response(msg):
+    a=open('lexicon-luke','rb')
+    successorlist=pickle.load(a)
+    a.close()
+    def nextword(a):
+        if a in successorlist:
+            return random.choice(successorlist[a])
+        else:
+            return 'the'
+    #speech='':
+    speech=msg
+    s=random.choice(speech.split())
+    response=''
+    while True:
+        neword=nextword(s)
+        response+=' '+neword
+        s=neword
+        if neword[-1] in ',?!.':
+            break
+    return response
